14c08d190b20e4d5b18cce0825842a30776e82b6
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-07-01 14:10:48 +0300

    change map

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"runtime"
@@ -12,12 +14,36 @@ func main() {
 
 	pathToMCMap["windows"] = os.Getenv("APPDATA") + "\\.minecraft"
 	pathToMCMap["linux"] = "~/.minecraft"
-	pathToMCMap["macOS"] = "~/Library/Application Support/minecraft"
+	pathToMCMap["darwin"] = "~/Library/Application Support/minecraft"
 
 	pathToMC := pathToMCMap[runtime.GOOS]
 
 	if exists, err := minecraftExists(pathToMC); !exists || err != nil {
 		return
+	}
+
+	file, err := os.Open(pathToMC + "\\options.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, 32*1024) // define your buffer size here.
+
+	for {
+		n, err := file.Read(buf)
+
+		if n > 0 {
+			fmt.Printl((buf[:n]) // your read buffer.
+		}
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("read %d bytes: %v", n, err)
+			break
+		}
 	}
 
 }
@@ -50,4 +76,10 @@ func minecraftExists(path string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
